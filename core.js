9f09499dcb6df187efae39e4a857e6ebd1f0ce68
kainzeyg <kainzeyg@yandex.ru>
2021-06-08 12:36:24 +0300

    0.0.1

--- core.js
+++ core.js
@@ -1,2 +1,11 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const url = 'https://www.uralchem.ru/press/news/'
+
+axios.get(url)
+    .then(responce => {
+        console.log(responce.data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
